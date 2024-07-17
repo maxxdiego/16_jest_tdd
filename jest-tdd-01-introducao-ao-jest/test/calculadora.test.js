4a380eefa897fc80c67ad2a85178009b3008ca5e
maxx.diego <77diego.max@gmail.com>
2024-07-17 15:10:47 -0300

    @maxxdiego

--- jest-tdd-01-introducao-ao-jest/test/calculadora.test.js
+++ jest-tdd-01-introducao-ao-jest/test/calculadora.test.js
@@ -4,7 +4,6 @@
 // test("Deve cadastrar um usuário quando receber um e-mail X", () => {})
 // it("Should do something...", () => {})
 
-
 const app = require("../app.js")
 
 // describe() serve para organizar os testes em categorias
@@ -14,11 +13,13 @@ describe("Operações básicas", () => {
     // 2º PARÂMETRO COM A FUNÇÃO DO TESTE
     // test() ou it("Should do somenthing...")
 
+    // Testando soma da calculadora
     test("Deve retornar o valor 10 ao somar 5 + 5", () => {
-        let resultado = app.soma(5,2)
+        let resultado = app.soma(5,5)
         expect(resultado).toEqual(10)
     })
 
+    // Testando multiplicação da calculadora
     test("Deve retornar o valor 10 ao multiplicar 2 por 5", () => {
         let resultado = app.mult(2,5)
         expect(resultado).toEqual(10)
